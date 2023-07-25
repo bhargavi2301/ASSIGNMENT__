9cb05fb2d71261d5a00e621f6b5fbe74ccf4865a
bhargavi2301 <madhabhargavi99@gmail.com>
2023-07-25 13:55:36 +0530

    first

---
+++ C/IF.C.number.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int A,B;
+	printf("enter A");
+	scanf("%d", & A);
+    printf("enter B");
+    scanf("%d", & B);
+	
+    	if(A>B)
+	
+	  {
+		 printf("A is greter");
+      }
+      	else
+      {
+         printf("B is smallest");	
+	  }
+	
+    
+    
+ }
--- C/star234.cpp
+++ C/star234.cpp
@@ -1,15 +1,14 @@
 #include<stdio.h>
 int main()
 {
-	for(int i=0;i<4;i++)
+	for(int i=0;i<8;i++)
 	{
-		for(int j=4;j>i;j--)
-		{
+	
 		 for(int k=0;k<i;k++)
 		 {
 		 	printf(" * ");
 		 }
 		 printf("\n");
-      	}
+      	
 	}
 }
