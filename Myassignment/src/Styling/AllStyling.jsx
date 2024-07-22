b577f8b84882651323f39af655fdbbc2debe7709
bhargavi2301 <madhabhargavi99@gmail.com>
2024-07-22 15:08:20 +0530

    botsrap

---
+++ Myassignment/src/Styling/AllStyling.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Styling1 from "./Styling1";
+import Crud from "./Crud";
+
+export default function AllStyling() {
+  return (
+    <div>
+        <Styling1/>
+        <Crud/>
+
+    </div>
+  );
+}
